e86cebb4850a2ec0aabe39be8fd941186322edae
Paulius11 <paulius.gaizauskas@gmail.com>
2019-12-28 13:22:55 +0200

    Create helperFunctions.java
    Add getStringFromStringIndex function

---
+++ helperFunctions.java
@@ -0,0 +1,10 @@
+// Arrays
+
+
+
+// Strings
+   
+   // getStringFromStringIndex("Abcd", 1) -> "b" 
+    private static  String getStringFromStringIndex(String str1, int i) {
+        return String.valueOf(str1.charAt(i));
+    }
