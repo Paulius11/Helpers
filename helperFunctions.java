de2ae0d43c2f9eb7f70ed71cf887b557d1b99f26
Paulius11 <paulius.gaizauskas@gmail.com>
2019-12-29 18:46:14 +0200

    Add getIntegerElement method

--- helperFunctions.java
+++ helperFunctions.java
@@ -5,6 +5,14 @@
 // Strings
    
    // getStringFromStringIndex("Abcd", 1) -> "b" 
-    private static  String getStringFromStringIndex(String str1, int i) {
-        return String.valueOf(str1.charAt(i));
-    }
+ private static  String getStringFromStringIndex(String str1, int index) {
+     return String.valueOf(str1.charAt(index));
+ }
+
+
+// Integers
+
+   // getIntegerElement(518, 2) -> 8 
+private static int getIntegerElement(int intNumber, int index) {
+    return Character.getNumericValue(Integer.toString(intNumber).charAt(index));
+}
